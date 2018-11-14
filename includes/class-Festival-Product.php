418d7491ebb24f0c6ae8b85f7c2b961729ac5409
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-11-14 17:52:57 +0100

    save festival_times, TODO: DEBUG lockers

--- includes/class-Festival-Product.php
+++ includes/class-Festival-Product.php
@@ -10,13 +10,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
     // Put your plugin code here
     add_action( 'plugins_loaded', 'fe_register_festival_product_type' );
-    add_action( 'woocommerce_product_data_panels', 'fe_festival_product_options_product_tab_content');
-    add_action( 'woocommerce_process_product_meta', 'fe_save_festival_product_options_field' );
+    // add_action( 'woocommerce_product_data_panels', 'fe_festival_product_options_product_tab_content');
+    // add_action( 'woocommerce_process_product_meta', 'fe_save_festival_product_options_field' );
     add_action( 'woocommerce_single_product_summary', 'fe_festival_product_template', 60 );
 
     add_filter( 'product_type_selector', 'fe_add_festival_product_type' );
     add_filter( 'woocommerce_product_data_tabs', 'festival_product_tab' );
 
+    fe_hook_general_tab_fields();
 }
 
 function fe_register_festival_product_type() {
@@ -74,39 +75,6 @@ function festival_product_tab( $tabs) {
     // array_push($tabs['general']['class'], 'show_if_festival_product');
     return $tabs;
 }
-
-// function fe_festival_product_options_product_tab_content() {
-//     // Dont forget to change the id in the div with your target of your product tab
-//     ?>
-<!-- <div id='festival_product_options' class='panel woocommerce_options_panel'> -->
-<?php
-//         ?>
-<!-- <div class='options_group'> -->
-<?php
-//             woocommerce_wp_checkbox( array(
-//                 'id' 	=> '_enable_festival_product',
-//                 'label' => __( 'Enable Festival Produkt Product', 'wcpt' ),
-//             ) );
-//             woocommerce_wp_text_input( array(
-//                 'id'          => '_festival_product_price',
-//                 'label'       => __( 'Price', 'wcpt' ),
-//                     'placeholder' => '',
-//                     'desc_tip'    => 'true',
-//                     'description' => __( 'Enter Festival Produkt Price.', 'wcpt' ),
-//             ));
-//         ?>
-<!-- </div> -->
-    <!-- </div> -->
-<?php
-// }
-
-// function fe_save_festival_product_options_field( $post_id ) {
-//     $enable_festival_product = isset( $_POST['_enable_festival_product'] ) ? 'yes' : 'no';
-//     update_post_meta( $post_id, '_enable_festival_product', $enable_festival_product );
-//     if ( isset( $_POST['_festival_product_price'] ) ) :
-//         update_post_meta( $post_id, '_festival_product_price', sanitize_text_field( $_POST['_festival_product_price'] ) );
-//     endif;
-// }
 
 function hasProductAttributes() {
     // if not
@@ -223,8 +191,149 @@ function fe_festival_product_custom_js() {
 
 
 // all available woocommerce_ form fields can be found here: /woocommerce/includes/admin/wc-meta-box-functions.php
-// add_action( 'woocommerce_product_options_general_product_data', 'woo_add_festival_start' );
-// add_action( 'woocommerce_process_product_meta', 'woo_save_festival_start' );
+
 
 // /Applications/MAMP/htdocs/safeboxen/wp-content/plugins/woocommerce/includes/wc-product-functions.php
 // wc_get_product_visibility_options
+
+function woo_display_festival_times() 
+{
+    global $woocommerce;
+
+    echo '<div class="options_group">';
+    
+    woocommerce_wp_text_input(
+        [
+            'id' => '_festival_start',
+            'label' => __('Festivalstart:', 'festival-events'),
+            'placeholder' => '01.08.2019',
+            'desc_tip' => 'true',
+            'description' => __('Trage hier das Datum vom Start des Festivals ein.', 'festival-events'),
+            'type' => 'date'
+        ]
+    );
+    
+    woocommerce_wp_text_input(
+        [
+            'id' => '_festival_end',
+            'label' => __('Festivalende:', 'festival-events'),
+            'placeholder' => '08.08.2019',
+            'desc_tip' => 'true',
+            'description' => __('Trage hier das Datum vom Ende des Festivals ein.', 'festival-events'),
+            'type' => 'date'
+        ]
+    );
+    
+    echo '</div>';
+}
+
+function woo_save_festival_times( $post_id ) 
+{
+    // global $woocommerce;
+    $festivalStart = $_POST['_festival_start'];
+    $festivalEnd = $_POST['_festival_end'];
+    // add validation
+    // 
+    if (!empty( $festivalStart )) {
+        update_post_meta( $post_id , '_festival_start', esc_attr($festivalStart));
+    }
+    if (!empty( $festivalEnd )) {
+        update_post_meta( $post_id , '_festival_end', esc_attr($festivalEnd));
+    }
+}
+
+function woo_display_locations() 
+{
+	global $woocommerce, $thepostid, $post;
+
+    $value = get_post_meta( $thepostid, '_festival_locations', true );
+    $value_string = implode($value, ',');
+    echo '<div class="options_group">';
+    
+    woocommerce_wp_text_input(
+        [
+            'id' => '_festival_locations',
+            'label' => __('Standorte:', 'festival-events'),
+            'placeholder' => 'Plaze, Center',
+            'desc_tip' => 'true',
+            'description' => __('Trage hier Kommasepariert die Standorte ein.', 'festival-events'),
+            'type' => 'text',
+            'value' => $value_string
+        ]
+    );
+    
+    echo '</div>';
+}
+
+function woo_save_locations($post_id) 
+{
+    // global $woocommerce;
+    $festivalLocations = $_POST['_festival_locations'];
+    $festivalLocations_array = preg_split("/[,]+/", esc_attr($festivalLocations));
+    $festivalLocations_array = array_map(function($x) {
+        return trim($x);
+    }, $festivalLocations_array);
+    // add validation
+    // 
+    if (!count( $festivalLocations_array ) == 0) {
+        update_post_meta( $post_id , '_festival_locations', $festivalLocations_array);
+    }
+}
+
+function woo_display_lockers() 
+{
+    // foreach location create checkbox ...
+    global $woocommerce, $thepostid, $post;
+
+    $locations = get_post_meta( $thepostid, '_festival_locations', true );
+    $lockers = get_post_meta( $thepostid, '_lockers', true );
+    $basis = ["M" => 'no', "M HV" => 'no', "L" => 'no', "L HV" => 'no', "XL" => 'no', "XL HV" => 'no'];
+    if (count($lockers) > 0) {
+
+    }
+    
+    echo '<div class="options_group">';
+    echo '<h2>Schließfächer</h2>';
+        for ($i=0; $i < count($locations); $i++) { 
+            echo "<h3>{$locations[$i]}</h3>";
+            // for($y = 0; $y < count($baseArray); $y++) {
+            $indexForLocker = 0;
+            foreach($basis AS $key => $locker) {
+                $lockerValue = $indexForLocker + 1;
+                woocommerce_wp_checkbox(array(
+                    'id' => "_lockers[{$i}][{$lockerValue}]",
+                    'label' => __($key, 'festival-events'),
+                    'description' => __('vorhanden?', 'festival-events'),
+                    'value' => 'yes',
+                    'cbvalue' => 'yes'
+                ));
+                $indexForLocker++;
+            }
+        }
+    echo '</div>';
+    // woocommerce_wp_checkbox(array(
+    //     'id' => '_'
+    // ))
+}
+
+function woo_save_lockers($post_id) 
+{
+    $lockersWithLocation = $_POST['_lockers'];
+    if (!count( $lockersWithLocation ) == 0) {
+        update_post_meta( $post_id , '_lockers', $lockersWithLocation);
+    }
+}
+
+function fe_hook_general_tab_fields() {
+    // festival times
+    add_action( 'woocommerce_product_options_general_product_data', 'woo_display_festival_times' );
+    add_action( 'woocommerce_process_product_meta', 'woo_save_festival_times' );
+
+    // location
+    add_action( 'woocommerce_product_options_general_product_data', 'woo_display_locations' );
+    add_action( 'woocommerce_process_product_meta', 'woo_save_locations' );
+
+    // lockers
+    add_action( 'woocommerce_product_options_general_product_data', 'woo_display_lockers' );
+    add_action( 'woocommerce_process_product_meta', 'woo_save_lockers' );
+}
