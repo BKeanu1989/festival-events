9c3f8faccbaa8db5a8353be9eb9cf0eb336f85da
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-11-29 13:54:04 +0100

    'readd' excerpt

--- includes/class-Festival-Product.php
+++ includes/class-Festival-Product.php
@@ -555,7 +555,7 @@ function fe_rebuild_woocommerce() {
 
     // remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
     // add_action( 'woocommerce_single_product_summary', 'fe_template_single_title', 5 );
-    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+    // remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
     remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
     // might still not work
