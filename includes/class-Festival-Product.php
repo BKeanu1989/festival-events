b3a17bd38cbee49618625d662ade22a6000ad358
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-11-29 13:01:29 +0100

    remove product_meta from markup

--- includes/class-Festival-Product.php
+++ includes/class-Festival-Product.php
@@ -556,10 +556,10 @@ function fe_rebuild_woocommerce() {
     // remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
     // add_action( 'woocommerce_single_product_summary', 'fe_template_single_title', 5 );
     // add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
     
     // might still not work
     remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
-
     add_action('woocommerce_before_single_product_summary', 'fe_show_product_image', 20);
 
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs' , 10);
