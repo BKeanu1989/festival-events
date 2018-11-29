08e580b5d2e10c62b838ae1fe061b3d2d111e576
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-11-29 13:37:34 +0100

    add markup to banner & hard coded banner text TODO: implement dynamically

--- templates/single-product/product-image.php
+++ templates/single-product/product-image.php
@@ -19,11 +19,22 @@
 defined( 'ABSPATH' ) || exit;
 
 global $product;
+$product_id = $product->get_id();
+// TODO: implement banner text
+$bannerText = get_post_meta($product_id, '_banner_text',true);
+$bannerText = "Dein Schließfach für das Wacken Open Air 2019";
 
 $post_thumbnail_id = $product->get_image_id();
 $img_src = wp_get_attachment_image_url($post_thumbnail_id, 'full');
 $img_srcset = wp_get_attachment_image_srcset($post_thumbnail_id, 'full');
 ?>
-<img src="<?php echo esc_url( $img_src ); ?>"
-srcset="<?php echo esc_attr( $img_srcset ); ?>"
-sizes="(max-width: 50em) 87vw, 680px" alt="A rad wolf">
+<div class="banner__wrapper">
+    <div class="banner__text">
+        <p class="banner__text--wrapper">
+            <?php echo $bannerText; ?>
+        </p>
+    </div>
+    <img src="<?php echo esc_url( $img_src ); ?>"
+    srcset="<?php echo esc_attr( $img_srcset ); ?>"
+    >
+</div>
