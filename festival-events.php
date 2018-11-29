8d301d3a15ebba55d995587e83f89f05af48d106
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-11-29 22:03:48 +0100

    only include css if is product

--- festival-events.php
+++ festival-events.php
@@ -38,9 +38,11 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 }
 
 function fe_add_styles() {
-    wp_enqueue_style('festival-events', plugins_url('/dist/css/festival-events.css', __FILE__), [], false, 'all');
-    // <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css');
+    if (is_product()) {
+        wp_enqueue_style('festival-events', plugins_url('/dist/css/festival-events.css', __FILE__), [], false, 'all');
+        // <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css');
+    }
 }
 
 
