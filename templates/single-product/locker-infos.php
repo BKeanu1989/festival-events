847b80929af4053bc56220a3b6f8551f2dcd48fb
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-11-29 10:02:16 +0100

    rename locker-sing-info.php

---
+++ templates/single-product/locker-infos.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * 
+ * @param 
+ */
+
+global $lockerDescription, $withOptional;
+global $variation;
+global $lockers;
+
+$lockers = getLockers($variations);
+$uniqueLockers = array_unique($lockers);
+
+$lockerInfo = [
+"M" => ["Außenmaß: 18cm x 12cm x 35cm (BxHxT), Innenmaß: 25cm x 14cm x 35 cm (BxHxT)", "passend für Wertsachen, kleine Tasche, kleine elektronische Geräte", "Steckdose max. 15 Watt", "Zum Laden von Handy, Kamera, Powerbank"],
+"L" => ["Außenmaß: 18cm x 40cm x 35cm (BxHxT), Innenmaß: 25cm x 42cm x 35 cm (BxHxT)", "passend für Wertsachen, Tasche/Rucksack, elektronische Geräte", "Steckdose max. 15 Watt", "Zum Laden von Handy, Kamera, Powerbank"],
+"M HV" => ["Außenmaß: 18cm x 12cm x 35cm (BxHxT), Innenmaß: 25cm x 14cm x 35 cm (BxHxT)", "passend für Wertsachen, kleine Tasche, kleine elektronische Geräte", "Steckdose max. 90 Watt", "Zum Laden von Notebook bis 11 Zoll, Soundbox."], 
+"L HV" => ["Außenmaß: 18cm x 40cm x 35cm (BxHxT), Innenmaß: 25cm x 42cm x 35 cm (BxHxT)", "passend für Wertsachen, Tasche/Rucksack, elektronische Geräte", "Steckdose max. 90 Watt", "Zum Laden von Notebook bis 15 Zoll, Soundbox."], 
+"XL" => ["", "", "", ""], 
+"XL HV" => ["", "", "", ""]];
+?>
+<div class="card__container">
+    <?php 
+        foreach($uniqueLockers as $key => $value) { ?>
+            <div class="card">
+                <div class="card__title">
+                    <?php echo $value; ?>
+                </div>
+                <div class="card__body">
+                    <?php
+                        foreach($lockerInfo[$value] AS $description) { ?>
+                            <div class="card__body--description">
+                                <?php echo $description; ?>
+                            </div>
+                        <?php }
+                    ?>
+                </div>
+                <div class="card__footer">
+                </div>
+            </div>
+        <?php } ?>
+</div>
