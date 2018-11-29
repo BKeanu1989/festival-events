4b41a82117e47a90f449d16211c9cc6cfb946047
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-11-29 11:33:45 +0100

    add :watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+
+var sassDir = './assets/scss/**/*.scss';
+
 sass.compiler = require('node-sass');
- 
+
 gulp.task('sass', function () {
-  return gulp.src('./assets/scss/**/*.scss')
+  return gulp.src(sassDir)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch(sassDir, ['sass']);
 });
