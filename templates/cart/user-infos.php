9ba5b91e46e4d2dcd393ae9a36f9579e1b6b3cd1
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-02 00:36:00 +0100

    set item key as base for html markup

--- templates/cart/user-infos.php
+++ templates/cart/user-infos.php
@@ -9,16 +9,16 @@ $items = $woocommerce->cart->get_cart();
 foreach($items AS $key => $item) {
     // var_dump($item);
     // var_dump($value);
-    write_log($item);
+    // write_log($item);
     $quantity = $item['quantity'];
     echo '<div class="woocommerce-custom-fields">';
         echo '<h3>' . __('Persönliche Daten für das/die Schließfächer', 'festival-events') . '</h3>';
         echo '<div class="woocommerce-custom-fields__field_wrapper">';
             for($i = 0; $i < $quantity; $i++) {
                 echo '<p class="form-row form-row-wide validate-required">';
-                    echo '<label for="name['.$key.']['.$i.']">' . __('Name', 'festival-events') . ' <abbr class="required" title="'.__('erforderlich', 'festival-events').'">*</abbr></label>';
+                    echo '<label for="'.$key.'[name]['.$i.']">' . __('Name', 'festival-events') . ' <abbr class="required" title="'.__('erforderlich', 'festival-events').'">*</abbr></label>';
                     echo '<span class="woocommerce-input-wrapper">';
-                        echo '<input type="text" class="input-text" id="name['.$key.']['.$i.']" name="name['.$key.']['.$i.']" required>';
+                        echo '<input type="text" class="input-text" id="'.$key.'[name]['.$i.']" name="'.$key.'[name]['.$i.']" required>';
                     echo '</span>';
                 echo '</p>';
             }
