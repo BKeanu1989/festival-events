a8ea8bddcc2c9b3bae96b7ad7d8e38d811d7caa4
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-02 00:36:21 +0100

    custom attributes get saved to db

--- includes/set-data-checkout.php
+++ includes/set-data-checkout.php
@@ -1,28 +1,19 @@
 <?php
 
 function fe_add_custom_data_to_order_item( $item, $cart_item_key, $values, $order ) {
-    write_log("values:");
-    write_log("really never gets called at live site:");
-    write_log($values);
-    $posted = $_POST;
-
-    write_log("POSTED:");
-    write_log($_POST);
-
-    // foreach( $item as $cart_item_key=>$values ) {
-    //     //TODO: format date
-    //     if( isset( $values[MK_DATE_OF_MESSAGE] ) ) {
-    //         $item->add_meta_data( __( MK_DATE_OF_MESSAGE, 'mokka-digital-greet' ), $values[MK_DATE_OF_MESSAGE], true );
-    //     }
-    
-    //     // TODO: truncate
-    //     if( isset( $values[MK_MESSAGE] ) ) {
-    //         $item->add_meta_data( __( MK_MESSAGE, 'mokka-digital-greet' ), $values[MK_MESSAGE], true );
-    //     }
-
-    //     if( isset( $values[MK_MOBIL_NUMBER] ) ) {
-    //         $item->add_meta_data( __( MK_MOBIL_NUMBER, 'mokka-digital-greet' ), $values[MK_MOBIL_NUMBER], true );
-    //     }
-    // }
+    foreach( $item as $cart_item_key => $cart_item_values ) {
+        $key = (isset($cart_item_values['key'])) ? $cart_item_values['key'] : '';
+        if (! isset($_POST[$key])) continue;
+        $quantity = $cart_item_values['quantity'];
+        for($i = 0; $i < $quantity; $i++) {
+            $name_for_locker = $_POST[$key]['name'][$i];
+            if ($quantity > 1) {
+                $item->add_meta_data("_multiple_values_bool", 1, true);
+                $item->add_meta_data("_name_for_locker--{$quantity}", $name_for_locker, true);
+            } else {
+                $item->add_meta_data('_name_for_locker', $name_for_locker, true);
+            }
+        }
+    }
 }
 add_action( 'woocommerce_checkout_create_order_line_item', 'fe_add_custom_data_to_order_item', 10, 4 );
