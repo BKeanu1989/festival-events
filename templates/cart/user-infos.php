a2470226485a8d233cdacd2d2d48f30a32a1220b
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-01 23:34:25 +0100

    add first simplified markup for further testing

--- templates/cart/user-infos.php
+++ templates/cart/user-infos.php
@@ -3,5 +3,26 @@ defined( 'ABSPATH' ) || exit;
 
 global $woocommerce;
 $items = $woocommerce->cart->get_cart();
-var_dump($items);
-echo "TEST";
+// var_dump($items);
+// echo "TEST";
+
+foreach($items AS $key => $item) {
+    // var_dump($item);
+    // var_dump($value);
+    write_log($item);
+    $quantity = $item['quantity'];
+    echo '<div class="woocommerce-custom-fields">';
+        echo '<h3>' . __('Persönliche Daten für das/die Schließfächer', 'festival-events') . '</h3>';
+        echo '<div class="woocommerce-custom-fields__field_wrapper">';
+            for($i = 0; $i < $quantity; $i++) {
+                echo '<p class="form-row form-row-wide validate-required">';
+                    echo '<label for="name['.$key.']['.$i.']">' . __('Name', 'festival-events') . ' <abbr class="required" title="'.__('erforderlich', 'festival-events').'">*</abbr></label>';
+                    echo '<span class="woocommerce-input-wrapper">';
+                        echo '<input type="text" class="input-text" id="name['.$key.']['.$i.']" name="name['.$key.']['.$i.']" required>';
+                    echo '</span>';
+                echo '</p>';
+            }
+        echo '</div>';
+    echo '</div>';
+}
+
