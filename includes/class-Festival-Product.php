15f5c88b3fc9853c8dd56f9233523b1fe7530a8e
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-01 14:10:08 +0100

    include own title file for product

--- includes/class-Festival-Product.php
+++ includes/class-Festival-Product.php
@@ -580,8 +580,8 @@ function fe_rebuild_woocommerce() {
 
     remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
 
-    // remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-    // add_action( 'woocommerce_single_product_summary', 'fe_template_single_title', 5 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+    add_action( 'woocommerce_single_product_summary', 'fe_template_single_title', 5 );
     // remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
     remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
