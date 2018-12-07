55fa65cc9ed2889308f6be16fb35d4809b364b08
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-07 19:22:33 +0100

    install & remove mutationobserver

--- assets/js/fe-custom-admin.js
+++ assets/js/fe-custom-admin.js
@@ -1,38 +1,80 @@
-let prices, variations;
+let prices, variationsWrapper, variationsTab, triggerPopulateButton;
 prices = Array.from(document.querySelectorAll('[name^="variable_regular_price"]'));
+variationsTab = document.querySelector('.variations_tab');
+triggerPopulateButton = document.querySelector('#trigger_populate_prices');
+variationsWrapper = document.querySelector('#variable_product_options_inner > div.woocommerce_variations');
 
-// can i use safari "Array.from"
-// prototyping
-prices.forEach((x) => x.value = '5');
+triggerPopulateButton.addEventListener('click', () => {
+    console.log("populate prices!!!");
+    console.log(variationsWrapper);
 
-// attribute_schliessfaecher[0]
-
-variations = document.querySelector('#variable_product_options_inner > div.woocommerce_variations');
-console.log(variations);
-
-// Options for the observer (which mutations to observe)
-var config = { attributes: false, childList: true, subtree: true };
-
-// Callback function to execute when mutations are observed
-var callback = function(mutationsList, observer) {
-    for(var mutation of mutationsList) {
-        if (mutation.type == 'childList') {
-            console.log('A child node has been added or removed.');
-        }
+    let priceSetter = new PriceSetter(variationsWrapper);
+    if ((variationsWrapper.querySelectorAll('.woocommerce_variation')).length == 0) {
+        priceSetter.installObserver();
+    } else {
+        priceSetter.populatePrices();
     }
-};
+})
 
-// Create an observer instance linked to the callback function
-var observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(variations, config);
-
-// Later, you can stop observing
-// observer.disconnect();
+console.log(variationsWrapper);
 
 class PriceSetter {
-    constructor() {
+    constructor(variationsWrapper) {
+        this.observerInstalled = false;
+        this.variationsWrapper = variationsWrapper;
+        this.lockerPrices = getDomLockerPrices();
+    }
+    
+    populatePrices(mutationsList, observer) {
+        console.log("all mutations done");
+        this.variations = Array.from(this.variationsWrapper.querySelectorAll('.woocommerce_variation'));
+        this.variations.forEach(variation => {
+            console.log(variation);
+        });
+        if (this.observerInstalled) this.removeObserver();
+    }
 
+    removeObserver() {
+        console.log("remove observer");
+        this.observer.disconnect();
+    }
+    
+    installObserver() {
+        console.log("install observer");
+        this.config = { attributes: false, childList: true, subtree: true };
+        this.observer = new MutationObserver(this.populatePrices.bind(this));
+        this.observer.observe(variationsWrapper, this.config);
+        this.observerInstalled = true;
+    }
+
+    getDomLockerPrices() {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+function simulateClick(element) {
+    var event = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+    // var cb = document.getElementById('checkbox'); 
+    let clicked = element.dispatchEvent(event);
+    if (clicked) {
+        alert("not cancelled");
+        // A handler called preventDefault.
+    } else {
+        // None of the handlers called preventDefault.
+        alert("cancelled");
     }
 }
