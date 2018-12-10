0de060721037bf4ef447a694cd0c0e58949c0ba5
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-10 18:42:04 +0100

    setting attributes via ajax working

--- includes/ajax-functions.php
+++ includes/ajax-functions.php
@@ -23,67 +23,73 @@ function fe_set_prices() {
 
 function fe_set_product_atts( ) {
     if (isset($_REQUEST)) {
-        $data = $_REQUEST["data"];
-
-        $post_id = $data["ID"];
-        $festivalStart = $data["FestivalStart"];
-        $festivalEnd = $data["FestivalEnd"];
-        $enumerateDays = $data["EnumerateDays"];
-
-        $lockers = $data["Lockers"];
-        $locations = $data["Locations"];
-
-        $visible   = '1';
-        $variation = '1';
+        try {
+            $data = $_REQUEST["data"];
     
+            $post_id = $data["ID"];
+            $festivalStart = $data["FestivalStart"];
+            $festivalEnd = $data["FestivalEnd"];
+            $enumerateDays = $data["EnumerateDays"];
     
-        global $wpdb;
-        $attributes = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}woocommerce_attribute_taxonomies WHERE attribute_name IN ('locker', 'period', 'location');" );
+            $lockers = $data["Lockers"];
+            $locations = $data["Locations"];
     
-        list($lockerAttributes, $periodAttributes, $locationAttributes) = $attributes;
-        // $position   = 0;  // Auto incremented position value starting at '0'
-        $data_attributes       = array(); // initialising (empty array)
-
-        write_log($attributes);
-
+            $visible   = '1';
+            $variation = '1';
         
-        setDataForAttribute($data_attributes, $lockerAttributes, $lockers);
-        setDataForPeriods($data_attributes, $periodAttributes, [$festivalStart, $festivalEnd, $enumerateDays]);
-        setDataForAttribute($data_attributes, $locationAttributes, $locations);
-        // setDataForAttribute($data_attributes, $periodAttributes, $lockers);
-        // Loop through each exiting product attribute
-        // foreach( $attributes as $attribute ){
-        //     // Get the correct taxonomy for product attributes
-        //     $taxonomy = 'pa_'.$attribute->attribute_name;
-        //     // $taxonomy = $attribute->attribute_name;
-        //     $attribute_id = $attribute->attribute_id;
+        
+            global $wpdb;
+            $attributes = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}woocommerce_attribute_taxonomies WHERE attribute_name IN ('locker', 'period', 'location');" );
+        
+            list($lockerAttributes, $periodAttributes, $locationAttributes) = $attributes;
+            // $position   = 0;  // Auto incremented position value starting at '0'
+            $data_attributes       = array(); // initialising (empty array)
+    
             
-        //     // Get all term Ids values for the current product attribute (array)
-        //     $term_ids = get_terms(array('taxonomy' => $taxonomy, 'fields' => 'ids', 'hide_empty' => false));
-    
-        //     // Get an empty instance of the WC_Product_Attribute object
-        //     $product_attribute = new WC_Product_Attribute();
-    
-        //     // Set the related data_attributes in the WC_Product_Attribute object
-        //     $product_attribute->set_id( $attribute_id );
-        //     $product_attribute->set_name( $taxonomy );
-        //     $product_attribute->set_options( $term_ids );
-        //     $product_attribute->set_position( $position );
-        //     $product_attribute->set_visible( $visible );
-        //     $product_attribute->set_variation( $variation );
-    
-        //     // Add the product WC_Product_Attribute object in the data_attributes array
-        //     $data_attributes[$taxonomy] = $product_attribute;
-    
-        //     $position++; // Incrementing position
-        // }
-        // Get an instance of the WC_Product object
-        $product = wc_get_product( $post_id );
-    
-        // Set the array of WC_Product_Attribute objects in the product
-        // $product->set_attributes( $data_attributes );
-    
-        // $product->save(); // Save the product
+            setDataForAttribute($data_attributes, $lockerAttributes, $lockers);
+            setDataForPeriods($data_attributes, $periodAttributes, [$festivalStart, $festivalEnd, $enumerateDays]);
+            setDataForAttribute($data_attributes, $locationAttributes, $locations);
+
+            // Loop through each exiting product attribute
+            // foreach( $attributes as $attribute ){
+            //     // Get the correct taxonomy for product attributes
+            //     $taxonomy = 'pa_'.$attribute->attribute_name;
+            //     // $taxonomy = $attribute->attribute_name;
+            //     $attribute_id = $attribute->attribute_id;
+                
+            //     // Get all term Ids values for the current product attribute (array)
+            //     $term_ids = get_terms(array('taxonomy' => $taxonomy, 'fields' => 'ids', 'hide_empty' => false));
+        
+            //     // Get an empty instance of the WC_Product_Attribute object
+            //     $product_attribute = new WC_Product_Attribute();
+        
+            //     // Set the related data_attributes in the WC_Product_Attribute object
+            //     $product_attribute->set_id( $attribute_id );
+            //     $product_attribute->set_name( $taxonomy );
+            //     $product_attribute->set_options( $term_ids );
+            //     $product_attribute->set_position( $position );
+            //     $product_attribute->set_visible( $visible );
+            //     $product_attribute->set_variation( $variation );
+        
+            //     // Add the product WC_Product_Attribute object in the data_attributes array
+            //     $data_attributes[$taxonomy] = $product_attribute;
+        
+            //     $position++; // Incrementing position
+            // }
+            // Get an instance of the WC_Product object
+            $product = wc_get_product( $post_id );
+        
+            // Set the array of WC_Product_Attribute objects in the product
+            // write_log("data attributes working");
+            // write_log($data_attributes);
+            $product->set_attributes( $data_attributes );
+        
+            $saved = $product->save(); // Save the product
+            write_log($saved);
+        } catch(Exception $e) {
+            echo 'Exception: '. $e->getMessage();
+            die;
+        }
     }
 }
 
@@ -127,6 +133,9 @@ function setDataForAttribute(&$data_attributes, $attribute, $whiteListed)
 {
     // TODO: make it 'nicer'
     $position = 2;
+    $visible = 1;
+    $variation = 1;
+
     if ($attribute->attribute_name === 'pa_location') $position = 0;
     // Get the correct taxonomy for product attributes
     $taxonomy = 'pa_'.$attribute->attribute_name;
@@ -135,11 +144,11 @@ function setDataForAttribute(&$data_attributes, $attribute, $whiteListed)
     
     // Get all term Ids values for the current product attribute (array)
     $term_ids = get_terms(array('taxonomy' => $taxonomy, 'hide_empty' => false));
-    $filteredTermIDs = array_filter($term_ids, function($id) use ($whiteListed) {
+    $filteredTermIDs = array_column(array_filter($term_ids, function($id) use ($whiteListed) {
         if (in_array($id->name, $whiteListed)) {
             return $id;
         }
-    });
+    }), 'term_id');
 
     // Get an empty instance of the WC_Product_Attribute object
     $product_attribute = new WC_Product_Attribute();
@@ -167,7 +176,8 @@ function setDataForPeriods(&$data_attributes, $attribute, $dateInfo) {
 
     $whiteListed = [];
     $whiteListed[] = 'Full Festival';
-
+    $visible = 1;
+    $variation = 1;
 
     list($festivalStart, $festivalEnd, $enumerateDays) = $dateInfo;
     $position = 1;
@@ -181,11 +191,11 @@ function setDataForPeriods(&$data_attributes, $attribute, $dateInfo) {
 
     // Get all term Ids values for the current product attribute (array)
     $term_ids = get_terms(array('taxonomy' => $taxonomy, 'hide_empty' => false));
-    $filteredTermIDs = array_filter($term_ids, function($id) use ($whiteListed) {
+    $filteredTermIDs = array_column(array_filter($term_ids, function($id) use ($whiteListed) {
         if (in_array($id->name, $whiteListed)) {
             return $id;
         }
-    });
+    }), 'term_id');
 
     // Get an empty instance of the WC_Product_Attribute object
     $product_attribute = new WC_Product_Attribute();
