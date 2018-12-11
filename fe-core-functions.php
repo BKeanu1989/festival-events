b1576f8ddca24fa8e4f5d5304f5d68ab0efb488e
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-11 10:27:21 +0100

    add array functions

--- fe-core-functions.php
+++ fe-core-functions.php
@@ -8,3 +8,36 @@ if ( ! function_exists('write_log')) {
         }
     }
 }
+
+// if (! function_exists('fe_multi_array_flatten')) {
+//     function fe_multi_array_flatten ($array) {
+//         $return = array();
+//         foreach ($array as $key => $value) {
+//             if (is_array($value)){
+//                 $return = array_merge($return, fe_multi_array_flatten($value));
+//             } else {
+//                 $return[$key] = $value;
+//             }
+//         }
+    
+//         return $return;
+//     }
+// }
+
+if (! function_exists('fe_array_flatten')) {
+    function fe_array_flatten ($array) {
+        $return = array();
+        array_walk_recursive($array, function ($singleArray) use (&$return) {
+            $return[] = $singleArray;
+        });
+    
+        return $return;
+    }
+}
+
+if (! function_exists('fe_return_first_array')) {
+    function fe_return_first_array($array) {
+        $keys = array_keys($array);
+        return $array[$keys[0]];
+    }
+}
