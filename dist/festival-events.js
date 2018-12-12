b566f65edb912b3555c16023ddacbfded6197028
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-12 21:40:51 +0100

    add concated ,babel, minified files

--- dist/festival-events.js
+++ dist/festival-events.js
@@ -1,68 +1,87 @@
-let prices, variationsWrapper, variationsTab, triggerPopulateButton;
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var prices = void 0,
+    variationsWrapper = void 0,
+    variationsTab = void 0,
+    triggerPopulateButton = void 0;
 prices = Array.from(document.querySelectorAll('[name^="variable_regular_price"]'));
 variationsTab = document.querySelector('.variations_tab');
 triggerPopulateButton = document.querySelector('#trigger_populate_prices');
 variationsWrapper = document.querySelector('#variable_product_options_inner > div.woocommerce_variations');
 
-
 if (triggerPopulateButton) {
-    triggerPopulateButton.addEventListener('click', () => {
+    triggerPopulateButton.addEventListener('click', function () {
         console.log("populate prices!!!");
-    
-        let priceSetter = new PriceSetter();
+
+        var priceSetter = new PriceSetter();
         priceSetter.populatePrices();
         console.log(priceSetter.variations);
-    })
+    });
     console.log(variationsWrapper);
 }
 
-class PriceSetter {
-    constructor() {
+var PriceSetter = function () {
+    function PriceSetter() {
+        _classCallCheck(this, PriceSetter);
+
         this.lockerPrices = this.getDomLockerPrices();
     }
-    
-    populatePrices(mutationsList, observer) {
-        jQuery.ajax({
-            url: ajaxurl,
-            data: {
-                'action': 'fe_set_prices',
-                // 'data': JSON.stringify()
-                'data': {lockerPrices: this.lockerPrices, productID: localizedVars.postID}
-            },
-            success: function(data) {
-                console.log(data);
-            },
-            error: function(err) {
-                console.log(err);
-            }
-        });
-    }
 
-    getDomLockerPrices() {
-        let lockerPrices = {};
-        lockerPrices["Full Festival"] = [];
-        lockerPrices["Daily"] = [];
-        let priceInputs = Array.from(document.querySelectorAll('[type="number"][name^="_schließf"]'));
-        priceInputs.forEach((x) => {
-            let obj = {};
-            let period = (x.dataset.period == 'Full Festival') ? 'Full Festival' : 'Daily';
-            obj.lockerType = x.dataset.lockertype;
-            obj.price = x.value;
-            obj.period = period;
-            lockerPrices[period].push(obj);
-        })
-        return lockerPrices;
-    }
-}
-class SetAttributes {
-    constructor() {
+    _createClass(PriceSetter, [{
+        key: 'populatePrices',
+        value: function populatePrices(mutationsList, observer) {
+            jQuery.ajax({
+                url: ajaxurl,
+                data: {
+                    'action': 'fe_set_prices',
+                    // 'data': JSON.stringify()
+                    'data': { lockerPrices: this.lockerPrices, productID: localizedVars.postID }
+                },
+                success: function success(data) {
+                    console.log(data);
+                },
+                error: function error(err) {
+                    console.log(err);
+                }
+            });
+        }
+    }, {
+        key: 'getDomLockerPrices',
+        value: function getDomLockerPrices() {
+            var lockerPrices = {};
+            lockerPrices["Full Festival"] = [];
+            lockerPrices["Daily"] = [];
+            var priceInputs = Array.from(document.querySelectorAll('[type="number"][name^="_schließf"]'));
+            priceInputs.forEach(function (x) {
+                var obj = {};
+                var period = x.dataset.period == 'Full Festival' ? 'Full Festival' : 'Daily';
+                obj.lockerType = x.dataset.lockertype;
+                obj.price = x.value;
+                obj.period = period;
+                lockerPrices[period].push(obj);
+            });
+            return lockerPrices;
+        }
+    }]);
+
+    return PriceSetter;
+}();
+
+var SetAttributes = function () {
+    function SetAttributes() {
+        _classCallCheck(this, SetAttributes);
+
         this.locations;
         this.$locations;
         this.festivalStart;
         this.$festivalStart;
         this.festivalEnd;
         this.$festivalEnd;
-        this.enumerateDays
+        this.enumerateDays;
 
         this.allLockers;
         this.$allLockers;
@@ -71,46 +90,56 @@ class SetAttributes {
         this.setValues();
     }
 
-    setValues() {
-        // this.enumerateDays = document.querySelector('[name="_enumerate_days"]').checked;
-        this.enumerateDays = (document.querySelector('[name="_enumerate_days"]').checked === true);
+    _createClass(SetAttributes, [{
+        key: 'setValues',
+        value: function setValues() {
+            // this.enumerateDays = document.querySelector('[name="_enumerate_days"]').checked;
+            this.enumerateDays = document.querySelector('[name="_enumerate_days"]').checked === true;
 
-        this.$locations = document.querySelector('[name="_festival_locations"]');
-        this.locations = this.$locations.value.trim().split(',');
-        
-        this.$festivalStart = document.querySelector('[name="_festival_start"]');
-        this.festivalStart = this.$festivalStart.value;
-        
-        this.$festivalEnd = document.querySelector('[name="_festival_end"]');
-        this.festivalEnd = this.$festivalEnd.value;
-        
-        this.$allLockers = document.querySelectorAll('[name^="_lockers"]');
-        this.allLockers = Array.from(this.$allLockers);
-        
-        this.lockers = this.allLockers.filter((x) => x.checked);
-        
-        this.lockers = this.lockers.map((x) => x.dataset.lockertype);
-    }
+            this.$locations = document.querySelector('[name="_festival_locations"]');
+            this.locations = this.$locations.value.trim().split(',');
 
-    ajaxCall() {
-        console.log("POSTID", localizedVars.postID);
-        jQuery.ajax({
-            url: ajaxurl,
-            data: {
-                'action': 'fe_set_product_atts',
-                // 'data': JSON.stringify()
-                'data': {ID: localizedVars.postID, Locations: this.locations, EnumerateDays: this.enumerateDays, FestivalStart: this.festivalStart, FestivalEnd: this.festivalEnd, Lockers: this.lockers}
-            },
-            success: function(data) {
-                console.log(data);
-                location.reload(true);
-            },
-            error: function(err) {
-                console.log(err);
-            }
-        });
-    }
-}
+            this.$festivalStart = document.querySelector('[name="_festival_start"]');
+            this.festivalStart = this.$festivalStart.value;
 
-const SET_ATTRIBUTES = new SetAttributes();
+            this.$festivalEnd = document.querySelector('[name="_festival_end"]');
+            this.festivalEnd = this.$festivalEnd.value;
+
+            this.$allLockers = document.querySelectorAll('[name^="_lockers"]');
+            this.allLockers = Array.from(this.$allLockers);
+
+            this.lockers = this.allLockers.filter(function (x) {
+                return x.checked;
+            });
+
+            this.lockers = this.lockers.map(function (x) {
+                return x.dataset.lockertype;
+            });
+        }
+    }, {
+        key: 'ajaxCall',
+        value: function ajaxCall() {
+            console.log("POSTID", localizedVars.postID);
+            jQuery.ajax({
+                url: ajaxurl,
+                data: {
+                    'action': 'fe_set_product_atts',
+                    // 'data': JSON.stringify()
+                    'data': { ID: localizedVars.postID, Locations: this.locations, EnumerateDays: this.enumerateDays, FestivalStart: this.festivalStart, FestivalEnd: this.festivalEnd, Lockers: this.lockers }
+                },
+                success: function success(data) {
+                    console.log(data);
+                    location.reload(true);
+                },
+                error: function error(err) {
+                    console.log(err);
+                }
+            });
+        }
+    }]);
+
+    return SetAttributes;
+}();
+
+var SET_ATTRIBUTES = new SetAttributes();
 SET_ATTRIBUTES.ajaxCall();
---
+++ festival-events.js
@@ -0,0 +1,116 @@
+let prices, variationsWrapper, variationsTab, triggerPopulateButton;
+prices = Array.from(document.querySelectorAll('[name^="variable_regular_price"]'));
+variationsTab = document.querySelector('.variations_tab');
+triggerPopulateButton = document.querySelector('#trigger_populate_prices');
+variationsWrapper = document.querySelector('#variable_product_options_inner > div.woocommerce_variations');
+
+
+if (triggerPopulateButton) {
+    triggerPopulateButton.addEventListener('click', () => {
+        console.log("populate prices!!!");
+    
+        let priceSetter = new PriceSetter();
+        priceSetter.populatePrices();
+        console.log(priceSetter.variations);
+    })
+    console.log(variationsWrapper);
+}
+
+class PriceSetter {
+    constructor() {
+        this.lockerPrices = this.getDomLockerPrices();
+    }
+    
+    populatePrices(mutationsList, observer) {
+        jQuery.ajax({
+            url: ajaxurl,
+            data: {
+                'action': 'fe_set_prices',
+                // 'data': JSON.stringify()
+                'data': {lockerPrices: this.lockerPrices, productID: localizedVars.postID}
+            },
+            success: function(data) {
+                console.log(data);
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        });
+    }
+
+    getDomLockerPrices() {
+        let lockerPrices = {};
+        lockerPrices["Full Festival"] = [];
+        lockerPrices["Daily"] = [];
+        let priceInputs = Array.from(document.querySelectorAll('[type="number"][name^="_schließf"]'));
+        priceInputs.forEach((x) => {
+            let obj = {};
+            let period = (x.dataset.period == 'Full Festival') ? 'Full Festival' : 'Daily';
+            obj.lockerType = x.dataset.lockertype;
+            obj.price = x.value;
+            obj.period = period;
+            lockerPrices[period].push(obj);
+        })
+        return lockerPrices;
+    }
+}
+class SetAttributes {
+    constructor() {
+        this.locations;
+        this.$locations;
+        this.festivalStart;
+        this.$festivalStart;
+        this.festivalEnd;
+        this.$festivalEnd;
+        this.enumerateDays
+
+        this.allLockers;
+        this.$allLockers;
+        this.lockers;
+
+        this.setValues();
+    }
+
+    setValues() {
+        // this.enumerateDays = document.querySelector('[name="_enumerate_days"]').checked;
+        this.enumerateDays = (document.querySelector('[name="_enumerate_days"]').checked === true);
+
+        this.$locations = document.querySelector('[name="_festival_locations"]');
+        this.locations = this.$locations.value.trim().split(',');
+        
+        this.$festivalStart = document.querySelector('[name="_festival_start"]');
+        this.festivalStart = this.$festivalStart.value;
+        
+        this.$festivalEnd = document.querySelector('[name="_festival_end"]');
+        this.festivalEnd = this.$festivalEnd.value;
+        
+        this.$allLockers = document.querySelectorAll('[name^="_lockers"]');
+        this.allLockers = Array.from(this.$allLockers);
+        
+        this.lockers = this.allLockers.filter((x) => x.checked);
+        
+        this.lockers = this.lockers.map((x) => x.dataset.lockertype);
+    }
+
+    ajaxCall() {
+        console.log("POSTID", localizedVars.postID);
+        jQuery.ajax({
+            url: ajaxurl,
+            data: {
+                'action': 'fe_set_product_atts',
+                // 'data': JSON.stringify()
+                'data': {ID: localizedVars.postID, Locations: this.locations, EnumerateDays: this.enumerateDays, FestivalStart: this.festivalStart, FestivalEnd: this.festivalEnd, Lockers: this.lockers}
+            },
+            success: function(data) {
+                console.log(data);
+                location.reload(true);
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        });
+    }
+}
+
+const SET_ATTRIBUTES = new SetAttributes();
+SET_ATTRIBUTES.ajaxCall();
