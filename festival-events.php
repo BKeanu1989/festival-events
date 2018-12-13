9241d054419c2bf202865e0db347899404890f95
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-13 13:26:28 +0100

    add same css file for frontend & admin

--- festival-events.php
+++ festival-events.php
@@ -41,12 +41,14 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 function fe_admin_scripts() {
 
     global $post;
+    wp_enqueue_style('festival-events', plugins_url('dist/css/festival-events.css', __FILE__), [], false, 'all');
     // $localizedVars = $post->ID;
     wp_register_script('fe-admin-script', plugins_url('dist/admin/festival-events.js', __FILE__), [], false, true);
     $localizedVars = [];
     if (!empty($post)) {
         $localizedVars = ['postID' => $post->ID];
     }
+
     wp_localize_script('fe-admin-script', 'localizedVars', $localizedVars);
     wp_enqueue_script('fe-admin-script');
     // wp_enqueue_style('fe-bulma', 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css');
@@ -54,7 +56,7 @@ function fe_admin_scripts() {
 
 function fe_add_styles() {
     // if (is_product()) {
-        wp_enqueue_style('festival-events', plugins_url('/dist/css/festival-events.css', __FILE__), [], false, 'all');
+        wp_enqueue_style('festival-events', plugins_url('dist/css/festival-events.css', __FILE__), [], false, 'all');
         // <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
         wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css');
     // }
