0322330a86206ade15ec330c7faf93307b147237
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-12-13 10:41:05 +0100

    trying to i10n array items

--- templates/single-product/locker-infos.php
+++ templates/single-product/locker-infos.php
@@ -5,20 +5,19 @@
  */
 defined( 'ABSPATH' ) || exit;
 
-global $lockerDescription, $withOptional;
+global $lockerDescription, $withOptional, $product;
 global $variation;
 global $lockers;
 
-$lockers = getLockers($variations);
-$uniqueLockers = array_unique($lockers);
+$lockers = array_map('trim', explode(',',$product->get_attribute('pa_locker')));
 
 $lockerInfo = [
-"M" => ["Außenmaß: 18cm x 12cm x 35cm (BxHxT), Innenmaß: 25cm x 14cm x 35 cm (BxHxT)", "passend für Wertsachen, kleine Tasche, kleine elektronische Geräte", "Steckdose max. 15 Watt", "Zum Laden von Handy, Kamera, Powerbank"],
-"L" => ["Außenmaß: 18cm x 40cm x 35cm (BxHxT), Innenmaß: 25cm x 42cm x 35 cm (BxHxT)", "passend für Wertsachen, Tasche/Rucksack, elektronische Geräte", "Steckdose max. 15 Watt", "Zum Laden von Handy, Kamera, Powerbank"],
-"M HV" => ["Außenmaß: 18cm x 12cm x 35cm (BxHxT), Innenmaß: 25cm x 14cm x 35 cm (BxHxT)", "passend für Wertsachen, kleine Tasche, kleine elektronische Geräte", "Steckdose max. 90 Watt", "Zum Laden von Notebook bis 11 Zoll, Soundbox."], 
-"L HV" => ["Außenmaß: 18cm x 40cm x 35cm (BxHxT), Innenmaß: 25cm x 42cm x 35 cm (BxHxT)", "passend für Wertsachen, Tasche/Rucksack, elektronische Geräte", "Steckdose max. 90 Watt", "Zum Laden von Notebook bis 15 Zoll, Soundbox."], 
+"M" => [__("Außenmaß: 18cm x 12cm x 35cm (BxHxT), Innenmaß: 25cm x 14cm x 35 cm (BxHxT)"), __("passend für Wertsachen, kleine Tasche, kleine elektronische Geräte"), __("Steckdose max. 15 Watt"), __("Zum Laden von Handy, Kamera, Powerbank")],
+"L" => [__("Außenmaß: 18cm x 40cm x 35cm (BxHxT), Innenmaß: 25cm x 42cm x 35 cm (BxHxT)"), __("passend für Wertsachen, Tasche/Rucksack, elektronische Geräte"), __("Steckdose max. 15 Watt"), __("Zum Laden von Handy, Kamera, Powerbank")],
+"M High-Voltage" => [__("Außenmaß: 18cm x 12cm x 35cm (BxHxT), Innenmaß: 25cm x 14cm x 35 cm (BxHxT)"), __("passend für Wertsachen, kleine Tasche, kleine elektronische Geräte"), __("Steckdose max. 90 Watt"), __("Zum Laden von Notebook bis 11 Zoll, Soundbox.")], 
+"L High-Voltage" => [__("Außenmaß: 18cm x 40cm x 35cm (BxHxT), Innenmaß: 25cm x 42cm x 35 cm (BxHxT)"), __("passend für Wertsachen, Tasche/Rucksack, elektronische Geräte"), __("Steckdose max. 90 Watt"), __("Zum Laden von Notebook bis 15 Zoll, Soundbox.")], 
 "XL" => ["", "", "", ""], 
-"XL HV" => ["", "", "", ""]];
+"XL High-Voltage" => ["", "", "", ""]];
 ?>
 <div class="wrapper">
     <h2 class="wrapper__title">
@@ -26,7 +25,7 @@ $lockerInfo = [
     </h2>
     <div class="card__container">
         <?php 
-            foreach($uniqueLockers as $key => $value) { ?>
+            foreach($lockers as $key => $value) { ?>
                 <div class="card locker_infos">
                     <div class="card__title">
                         <?php echo $value; ?>
