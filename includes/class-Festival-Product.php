63265edb1379a931e26a84bb7f9ad277a605b725
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-01-10 20:48:58 +0100

    remove coupon option from checkout

--- includes/class-Festival-Product.php
+++ includes/class-Festival-Product.php
@@ -743,6 +743,7 @@ function fe_rebuild_woocommerce() {
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs' , 10);
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display' , 15);
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products' , 20);
+    remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
     // * @hooked woocommerce_output_product_data_tabs - 10
     // * @hooked woocommerce_upsell_display - 15
     // * @hooked woocommerce_output_related_products - 20
