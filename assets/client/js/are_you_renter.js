f441be8ebab5dea523887b0a4f82c16966b5e91c
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-01-11 20:33:39 +0100

    implement validation of radio boxes via weakmap

--- assets/client/js/are_you_renter.js
+++ assets/client/js/are_you_renter.js
@@ -1,56 +1,102 @@
-let are_you_renter_container;
-let are_you_renter;
-let are_you_renter_set = false;
+let all_are_you_renter_container;
+let are_you_renter_boxes;
 let form_checkout;
+let fieldsToValidate;
+let uniqueIdentifiers = new Set();
 
-// are_you_renter_container = document.querySelector('#renter_field');
+all_are_you_renter_container = document.querySelectorAll('div.checkbox-required[data-identifier]');
+let wm_validationPassed = new WeakMap();
 
-// if (are_you_renter_container) {
-    are_you_renter = Array.from(document.querySelectorAll('input[type="radio"][name^="renter"'));
-    if (are_you_renter) {
-        are_you_renter.forEach((radioButton) => {
-            console.log(radioButton);
+if (all_are_you_renter_container) {
+    are_you_renter_boxes = Array.from(document.querySelectorAll('input[type="radio"][name^="renter"'));
+    if (are_you_renter_boxes) {
+        are_you_renter_boxes.forEach((radioButton) => {
+            let key = radioButton.dataset.identifier;
+            uniqueIdentifiers.add(key);
             radioButton.addEventListener('change', () => {
-                are_you_renter_set = true;
-                let value = radioButton.value;
-                let key = radioButton.dataset.identifier;
-                console.log("key:", key);
-                let findExtraPerson = document.querySelector(`.extra_person__wrapper[data-identifier="${key}"`);
-
-                if (value === 'yes') {
-                    // do nothing xD
-                    findExtraPerson.classList.add('hide_if_yes');
-                    findExtraPerson.classList.add('hide_if_default');
-                    findExtraPerson.classList.toggle('fadeIn');
-                }
-                if (value === 'no') {
-                    findExtraPerson.classList.remove('hide_if_yes');
-                    findExtraPerson.classList.remove('hide_if_default');
-                    findExtraPerson.classList.toggle('fadeIn');
+                try {
+                    let value = radioButton.value;
+                    let findExtraPerson = document.querySelector(`.extra_person__wrapper[data-identifier="${key}"`);
+    
+                    if (value === 'yes') {
+                        findExtraPerson.classList.add('hide_if_yes');
+                        findExtraPerson.classList.add('hide_if_default');
+                        findExtraPerson.classList.toggle('fadeIn');
+                    }
+                    if (value === 'no') {
+                        findExtraPerson.classList.remove('hide_if_yes');
+                        findExtraPerson.classList.remove('hide_if_default');
+                        findExtraPerson.classList.toggle('fadeIn');
+                    }
+                } catch(err) {
+                    console.log(err);
                 }
             })
         })
     }
-// }
+}
+
 form_checkout = document.querySelector('form[name="checkout"]');
 if (form_checkout) {
     jQuery( 'form.checkout' ).on( 'checkout_place_order', function() {
-        if (!are_you_renter_set) {
-            if (are_you_renter_container.classList.contains('blink')) {
-                are_you_renter_container.classList.remove('blink');
-            }
+        let validatedCheckboxes = validateCheckbox();
+        if (!validatedCheckboxes) {
+            try {
+                all_are_you_renter_container[0].scrollIntoView({behavior: 'smooth'});
+                all_are_you_renter_container.forEach((x) => {
+                    if (x.classList.contains('blink')) {
+                        console.log("remove blink class");
+                        x.classList.remove('blink');
+                    }
+                });
+                // blinking is set in css
+                setTimeout(() => {
+                    console.log(all_are_you_renter_container);
+                    all_are_you_renter_container.forEach((x) => {
+                        // console.log(x);
+                        if (!wm_validationPassed.get(x)) {
+                            x.classList.add('blink');
+                            console.log(x);
+                        }
+                    })
+                }, 500);
+                return false;
 
-            are_you_renter_container.scrollIntoView({behavior: 'smooth'});
-            // blinking is set in css
-            setTimeout(() => {
-                are_you_renter_container.classList.add('blink');
-            }, 500);
-            return false;
+            } catch(err) {
+                console.log(err);
+            }
         }
         return true;
     });
 }
 
+//TODO: validate all renter buttons
+// set for data-identifier (grouping)
+// if no value is set in a group --> fail validation
+function validateCheckbox() {
+    let validationArray = [];
+    let validationPassed;
+    uniqueIdentifiers.forEach((data_hash) => {
+        try {
+            let validationPassed = false;
+            let radioContainer = document.querySelector(`div[data-identifier="${data_hash}"]`);
+            let radiosPerGroup = Array.from(radioContainer.querySelectorAll('input[type="radio"]'));
+            validationPassed = radiosPerGroup.some((element) => {
+                return element.checked
+            });
+            wm_validationPassed.set(radioContainer, validationPassed);
+            validationArray.push(validationPassed);
+        } catch(err) {
+            console.log(err);
+        }
+    });
+    validationPassed = validationArray.every((x) => x);
+    return validationPassed;
+}
+
 //TODO: client side validation of extra fields
 // get all extra_person__wrapper fields without hide_if_yes hide_if_default
-let fieldsToValidate =  document.querySelectorAll('.extra_person__wrapper:not(.hide_if_yes)');
+fieldsToValidate = document.querySelectorAll('.extra_person__wrapper:not(.hide_if_yes)');
+if (fieldsToValidate) {
+
+}
--- dist/client/festival-events-frontend.js
+++ dist/client/festival-events-frontend.js
@@ -4,60 +4,107 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var are_you_renter_container = void 0;
-var are_you_renter = void 0;
-var are_you_renter_set = false;
+var all_are_you_renter_container = void 0;
+var are_you_renter_boxes = void 0;
 var form_checkout = void 0;
+var fieldsToValidate = void 0;
+var uniqueIdentifiers = new Set();
 
-// are_you_renter_container = document.querySelector('#renter_field');
+all_are_you_renter_container = document.querySelectorAll('div.checkbox-required[data-identifier]');
+var wm_validationPassed = new WeakMap();
 
-// if (are_you_renter_container) {
-are_you_renter = Array.from(document.querySelectorAll('input[type="radio"][name^="renter"'));
-if (are_you_renter) {
-    are_you_renter.forEach(function (radioButton) {
-        console.log(radioButton);
-        radioButton.addEventListener('change', function () {
-            are_you_renter_set = true;
-            var value = radioButton.value;
+if (all_are_you_renter_container) {
+    are_you_renter_boxes = Array.from(document.querySelectorAll('input[type="radio"][name^="renter"'));
+    if (are_you_renter_boxes) {
+        are_you_renter_boxes.forEach(function (radioButton) {
             var key = radioButton.dataset.identifier;
-            console.log("key:", key);
-            var findExtraPerson = document.querySelector('.extra_person__wrapper[data-identifier="' + key + '"');
+            uniqueIdentifiers.add(key);
+            radioButton.addEventListener('change', function () {
+                try {
+                    var value = radioButton.value;
+                    var findExtraPerson = document.querySelector('.extra_person__wrapper[data-identifier="' + key + '"');
 
-            if (value === 'yes') {
-                // do nothing xD
-                findExtraPerson.classList.add('hide_if_yes');
-                findExtraPerson.classList.add('hide_if_default');
-                findExtraPerson.classList.toggle('fadeIn');
-            }
-            if (value === 'no') {
-                findExtraPerson.classList.remove('hide_if_yes');
-                findExtraPerson.classList.remove('hide_if_default');
-                findExtraPerson.classList.toggle('fadeIn');
-            }
+                    if (value === 'yes') {
+                        findExtraPerson.classList.add('hide_if_yes');
+                        findExtraPerson.classList.add('hide_if_default');
+                        findExtraPerson.classList.toggle('fadeIn');
+                    }
+                    if (value === 'no') {
+                        findExtraPerson.classList.remove('hide_if_yes');
+                        findExtraPerson.classList.remove('hide_if_default');
+                        findExtraPerson.classList.toggle('fadeIn');
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+            });
         });
-    });
+    }
 }
-// }
+
 form_checkout = document.querySelector('form[name="checkout"]');
 if (form_checkout) {
     jQuery('form.checkout').on('checkout_place_order', function () {
-        if (!are_you_renter_set) {
-            if (are_you_renter_container.classList.contains('blink')) {
-                are_you_renter_container.classList.remove('blink');
+        var validatedCheckboxes = validateCheckbox();
+        if (!validatedCheckboxes) {
+            try {
+                all_are_you_renter_container[0].scrollIntoView({ behavior: 'smooth' });
+                all_are_you_renter_container.forEach(function (x) {
+                    if (x.classList.contains('blink')) {
+                        console.log("remove blink class");
+                        x.classList.remove('blink');
+                    }
+                });
+                // blinking is set in css
+                setTimeout(function () {
+                    console.log(all_are_you_renter_container);
+                    all_are_you_renter_container.forEach(function (x) {
+                        // console.log(x);
+                        if (!wm_validationPassed.get(x)) {
+                            x.classList.add('blink');
+                            console.log(x);
+                        }
+                    });
+                }, 500);
+                return false;
+            } catch (err) {
+                console.log(err);
             }
-
-            are_you_renter_container.scrollIntoView({ behavior: 'smooth' });
-            // blinking is set in css
-            setTimeout(function () {
-                are_you_renter_container.classList.add('blink');
-            }, 500);
-            return false;
         }
         return true;
     });
 }
 
+//TODO: validate all renter buttons
+// set for data-identifier (grouping)
+// if no value is set in a group --> fail validation
+function validateCheckbox() {
+    var validationArray = [];
+    var validationPassed = void 0;
+    uniqueIdentifiers.forEach(function (data_hash) {
+        try {
+            var _validationPassed = false;
+            var radioContainer = document.querySelector('div[data-identifier="' + data_hash + '"]');
+            var radiosPerGroup = Array.from(radioContainer.querySelectorAll('input[type="radio"]'));
+            _validationPassed = radiosPerGroup.some(function (element) {
+                return element.checked;
+            });
+            wm_validationPassed.set(radioContainer, _validationPassed);
+            validationArray.push(_validationPassed);
+        } catch (err) {
+            console.log(err);
+        }
+    });
+    validationPassed = validationArray.every(function (x) {
+        return x;
+    });
+    return validationPassed;
+}
+
 //TODO: client side validation of extra fields
+// get all extra_person__wrapper fields without hide_if_yes hide_if_default
+fieldsToValidate = document.querySelectorAll('.extra_person__wrapper:not(.hide_if_yes)');
+if (fieldsToValidate) {}
 var chooseLockerButtons = void 0,
     lockerSelect = void 0,
     productForm = void 0;
