5634433c6ebd32186b452950b2db9c4b6594a2fd
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-01-16 11:56:13 +0100

    change markup & style for card

--- templates/single-product/opening-items-and-locations.php
+++ templates/single-product/opening-items-and-locations.php
@@ -20,28 +20,13 @@ $get_opening_times_per_location = get_post_meta($product_id, '_opening_times');
             <div class="card__title">
                 <h2><?php _e('Allgemeine Informationen', 'festival-events'); ?></h2>
             </div>
-            <div class="card__image">
-                <!-- display image here -->
-                <!-- TODO: srcset-->
-                <!-- <img src="https://festivalsafeboxen.bigboxberlin.de/wp-content/uploads/2017/12/BigBoxBerlin_SafeBOX_WRS-7.jpg" alt=""> -->
-            </div>
             <div class="card__body">
-                <div class="card__body--item card__locations">
-                    <h3><i class="fas fa-users"></i><?php _e('Standorte', 'festival-events'); ?></h3>
-                    <ul>
-                    <?php foreach($get_opening_times_per_location[0] AS $key => $value) { ?>
-                        <?php echo "<li>". ucfirst($key) . "</li>" ?>
-                    <?php } ?>
-                    </ul>
-                </div>
-                <div class="card__body--item card__opening_times">
-                    <h3><i class="fas fa-door-open"></i><?php _e('Öffnungszeiten', 'festival-events') ?></h3>
-                    <ul>
-                    <?php foreach($get_opening_times_per_location[0] AS $key => $value) { ?>
-                        <?php echo "<li>". ucfirst($value) . "</li>" ?>
-                    <?php } ?>
-                    </ul>
-                </div>
+                <?php foreach($get_opening_times_per_location[0] AS $key => $value) { ?>
+                    <div class="card__body--item card__locations">
+                        <h3><?php _e('Standort', 'festival-events'); echo " " . ucfirst($key) ?></h3>
+                        <?php echo "<p>". ucfirst($value) . "</p>" ?>
+                    </div>
+                <?php } ?>
             </div>
         </div>
     </div>
