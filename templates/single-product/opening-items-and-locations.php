ddb9eef9e170d895d16a3f502657b5d3a27f0522
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-01-27 17:50:04 +0100

    foreach with 'iterator index'

--- templates/single-product/opening-items-and-locations.php
+++ templates/single-product/opening-items-and-locations.php
@@ -11,7 +11,7 @@ global $product;
 $product_id = $product->get_id();
 // $get_opening_times_per_location = get_post_meta($product_id, '_opening_items_per_location');
 $get_opening_times_per_location = get_post_meta($product_id, '_opening_times');
-
+$iterator = 1;
 // component
 ?>
 <div class="wrapper">
@@ -23,8 +23,10 @@ $get_opening_times_per_location = get_post_meta($product_id, '_opening_times');
             <div class="card__body">
                 <?php foreach($get_opening_times_per_location[0] AS $key => $value) { ?>
                     <div class="card__body--item card__locations">
-                        <h3><?php _e('Standort', 'festival-events'); echo " " . ucfirst($key) ?></h3>
-                        <?php echo "<p>". ucfirst($value) . "</p>" ?>
+                        <h3 style="text-align: center;"><?php _e('Standort', 'festival-events'); echo " " . $iterator ?></h3>
+                        <?php echo "<p style='font-weight:bold;'>". ucfirst($key) . "</p>" ?>
+                        <?php echo "<p style='font-weight:bold;'>". ucfirst($value) . "</p>" ?>
+                        <?php $iterator++; ?>
                     </div>
                 <?php } ?>
             </div>
