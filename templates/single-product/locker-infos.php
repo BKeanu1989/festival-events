c289d07a200a4e999d5dff74738e2adcaade6fcd
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-02-22 13:48:10 +0100

    add priority to lockers

--- templates/single-product/locker-infos.php
+++ templates/single-product/locker-infos.php
@@ -26,7 +26,8 @@ $lockerInfo = [
         __("max. 15 Watt", 'festival-events'),
         __("Ausreichend zum Laden von Handy, Kamera, Powerbank", 'festival-events')
     ],
-    __("Nicht erlaubt. Bitte halte Dich daran, da bei Überschreitung der 15 Watt die Sicherung rausfliegen wird.", 'festival-events')
+    __("Nicht erlaubt. Bitte halte Dich daran, da bei Überschreitung der 15 Watt die Sicherung rausfliegen wird.", 'festival-events'),
+    "priority" => 1
 ],
 "L" => [
     __("18 x 40 x 35 cm", 'festival-events'),
@@ -36,7 +37,8 @@ $lockerInfo = [
         __("max. 15 Watt", 'festival-events'),
         __("Ausreichend zum Laden von Handy, Kamera, Powerbank", 'festival-events'),
     ],
-    __("Nicht erlaubt. Bitte halte Dich daran, da bei Überschreitung der 15 Watt die Sicherung rausfliegen wird.", 'festival-events')
+    __("Nicht erlaubt. Bitte halte Dich daran, da bei Überschreitung der 15 Watt die Sicherung rausfliegen wird.", 'festival-events'),
+    "priority" => 3
 ],
 "M High-Voltage" => [
     __("18 x 12 x 35 cm", 'festival-events'),
@@ -46,7 +48,8 @@ $lockerInfo = [
         __("max. 90 Watt", 'festival-events'),
         __("Ausreichend zum Laden von Notebook bis 11 Zoll, Soundbox, gleichzeitiges Aufladen mehrerer Handys/Powerbanks", 'festival-events'),
     ],
-    __("Erlaubt, beachte bitte jedoch die max. Steckdosenleistung von 90 Watt.", 'festival-events')
+    __("Erlaubt, beachte bitte jedoch die max. Steckdosenleistung von 90 Watt.", 'festival-events'),
+    "priority" => 2
 ], 
 "L High-Voltage" => [
     __("18 x 40 x 35 cm", 'festival-events'),
@@ -56,7 +59,8 @@ $lockerInfo = [
         __("max. 90 Watt", 'festival-events'),
         __("Ausreichend zum Laden von Notebook bis 11 Zoll, Soundbox, gleichzeitiges Aufladen mehrerer Handys/Powerbanks", 'festival-events'),
     ],
-    __("Erlaubt, beachte bitte jedoch die max. Steckdosenleistung von 90 Watt.", 'festival-events')
+    __("Erlaubt, beachte bitte jedoch die max. Steckdosenleistung von 90 Watt.", 'festival-events'),
+    "priority" => 4
 ], 
 "XL" => [
     __("33 x 40 x 48 cm", 'festival-events'),
@@ -66,7 +70,8 @@ $lockerInfo = [
         __("max. 15 Watt", 'festival-events'),
         __("Ausreichend zum Laden von Handy, Kamera, Powerbank", 'festival-events'),
     ],
-    __("Nicht erlaubt. Bitte halte Dich daran, da bei Überschreitung der 15 Watt die Sicherung rausfliegen wird.", 'festival-events')
+    __("Nicht erlaubt. Bitte halte Dich daran, da bei Überschreitung der 15 Watt die Sicherung rausfliegen wird.", 'festival-events'),
+    "priority" => 5
 ], 
 "XL High-Voltage" => [
     __("33 x 40 x 48 cm", 'festival-events'),
@@ -76,14 +81,21 @@ $lockerInfo = [
         __("max. 90 Watt", 'festival-events'),
         __("Ausreichend zum Laden von Notebook bis 11 Zoll, Soundbox, gleichzeitiges Aufladen  mehrerer Handys/Powerbanks", 'festival-events'),
     ],
-    __("Erlaubt, beachte bitte jedoch die max. Steckdosenleistung von 90 Watt.", 'festival-events')
+    __("Erlaubt, beachte bitte jedoch die max. Steckdosenleistung von 90 Watt.", 'festival-events'),
+    "priority" => 6
 ]];
+
+function cmp($a, $b) 
+{
+    return ($a['priority'] < $b['priority']) ? -1 : 1;
+}
 
 function buildLockerInfo($lockers, $lockerInfo) {
     $givenLockers = [];
     foreach($lockers AS $key => $lockerName) {
         $givenLockers[$lockerName] = $lockerInfo[$lockerName];
     }
+    uasort($givenLockers, "cmp");
     return $givenLockers;
 }
 
@@ -97,7 +109,7 @@ $givenLockers = buildLockerInfo($lockers, $lockerInfo);
 
     <div class="card__container lockers">
         <?php foreach($givenLockers AS $lockerType => $lockerInfo) { 
-            list($outerDiameter, $innerDiameter, $suitability, $power, $useable) = $givenLockers[$lockerType];
+            list($outerDiameter, $innerDiameter, $suitability, $power, $useable, $priority) = $givenLockers[$lockerType];
         ?> 
             <div class="card">
                 <div class="card__title">
