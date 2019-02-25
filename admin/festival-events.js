094db5102ae6e54392aef9fb3ea4c2be07981ca3
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-02-25 13:58:26 +0100

    add logic for cleave in base file add logic for cleave in base file

--- admin/festival-events.js
+++ admin/festival-events.js
@@ -3,6 +3,30 @@ let buttons;
 buttons = Array.from(document.querySelectorAll('.foreign-language button'));
 
 buttons.forEach((x) => x.disabled = true);
+
+let $festivalStart;
+let $festivalEnd;
+
+$festivalStart = document.querySelector('#_festival_start');
+$festivalEnd = document.querySelector('#_festival_end');
+
+if ($festivalStart && $festivalEnd) {
+    try {
+        new Cleave('#_festival_start', {
+            date: true,
+            datePattern: ['Y', 'm', 'd'],
+            delimiter: '-'
+        })
+    
+        new Cleave('#_festival_end', {
+            date: true,
+            datePattern: ['Y', 'm', 'd'],
+            delimiter: '-'
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
 let prices, variationsWrapper, variationsTab, triggerPopulateButton;
 prices = Array.from(document.querySelectorAll('[name^="variable_regular_price"]'));
 variationsTab = document.querySelector('.variations_tab');
--- assets/admin/js/custom.js
+++ assets/admin/js/custom.js
@@ -3,3 +3,27 @@ let buttons;
 buttons = Array.from(document.querySelectorAll('.foreign-language button'));
 
 buttons.forEach((x) => x.disabled = true);
+
+let $festivalStart;
+let $festivalEnd;
+
+$festivalStart = document.querySelector('#_festival_start');
+$festivalEnd = document.querySelector('#_festival_end');
+
+if ($festivalStart && $festivalEnd) {
+    try {
+        new Cleave('#_festival_start', {
+            date: true,
+            datePattern: ['Y', 'm', 'd'],
+            delimiter: '-'
+        })
+    
+        new Cleave('#_festival_end', {
+            date: true,
+            datePattern: ['Y', 'm', 'd'],
+            delimiter: '-'
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
